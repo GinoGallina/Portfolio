109c6a19ffbb6f8cbf490a5045838b44525fe0c9
GinoGallina <ginogallina2002@gmail.com>
2024-02-18 17:38:22 -0300

    cambio en vite.config, base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Cargar las variables de entorno si es necesario
+import { config as dotenvConfig } from 'dotenv'
+dotenvConfig()
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base:
+        process.env.NODE_ENV === 'production'
+            ? process.env.VITE_PUBLIC_PATH
+            : '/'
 })
