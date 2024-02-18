df7a54903e81a4132e1f02a066af040306ff7b6c
GinoGallina <ginogallina2002@gmail.com>
2024-02-18 17:26:36 -0300

    dotenv

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 // Para el FORM Component
 import 'hamburgers/dist/hamburgers.min.css'
-// import 'select2/dist/css/select2.min.css'
-
-// import tilt from 'tilt.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
